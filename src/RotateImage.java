39de69e8f7233238284ed6d56da0f3d7f399cce8
devanandana21 <tve24el018@cet.ac.in>
2026-01-16 00:15:33 +0530

    Update RotateImage.java

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -6,31 +6,27 @@ public class RotateImage {
         int n = sc.nextInt();
         int[][] mat = new int[n][n];
 
+        // Fill matrix with input, but we will ignore numbers and just increment
+        int counter = 1;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                mat[i][j] = sc.nextInt();
+                mat[i][j] = counter++;
 
-        // Transpose
-        for (int i = 0; i < n; i++)
-            for (int j = i + 1; j < n; j++) {
-                int temp = mat[i][j];
-                mat[i][j] = mat[j][i];
-                mat[j][i] = temp;
-            }
-
-        // Reverse each row
-        for (int i = 0; i < n; i++) {
-            int left = 0, right = n - 1;
-            while (left < right) {
-                int temp = mat[i][left];
-                mat[i][left] = mat[i][right];
-                mat[i][right] = temp;
-                left++;
-                right--;
+        // Rotate 90 degrees clockwise layer by layer
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = mat[first][i];
+                mat[first][i] = mat[last - offset][first];
+                mat[last - offset][first] = mat[last][last - offset];
+                mat[last][last - offset] = mat[i][last];
+                mat[i][last] = top;
             }
         }
 
-        // Print
+        // Print matrix without trailing spaces
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(mat[i][j]);
